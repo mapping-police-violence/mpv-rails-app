4f1ba4390a5afb5154d853fcb5746e0cd032921e
Laura Kogler <lkogler@gmail.com>
2015-07-26 18:24:59 -0700

    [#99006252] Create CsvImporter class. Add Incident model to app. Add csv with initial data.

---
+++ app/helpers/csv_importer.rb
@@ -0,0 +1,42 @@
+require 'csv'
+
+class CsvImporter
+  def self.import filename
+    CSV.foreach filename do |row|
+      next if row[0] == "Victim's name"
+
+      Incident.create!({
+                          :victim_name => row[0],
+                          :victim_age => row[1],
+                          :victim_gender => row[3],
+                          :victim_race => row[4],
+                          :victim_image_url => row[5],
+                          :incident_date => row[6],
+                          :incident_street_address => row[7],
+                          :incident_city => row[8],
+                          :incident_state => row[9],
+                          :incident_zip => row[10],
+                          :incident_county => row[11],
+                          :agency_responsible => row[12],
+                          :cause_of_death => row[13],
+                          :alleged_victim_crime => row[14],
+                          :crime_category => row[15],
+                          :aggregate_crime_category => row[16],
+                          :caveat => row[17],
+                          :solution => row[18],
+                          :incident_description => row[19],
+                          :official_disposition_of_death => row[20],
+                          :criminal_charges => row[21],
+                          :news_url => row[22],
+                          :mental_illness => row[23],
+                          :unarmed => row[24],
+                          :line_of_duty => row[25],
+                          :note => row[26],
+                          :in_custody => row[27],
+                          :arrest_related_death => row[28],
+                          :unique_mpv => row[31],
+                      })
+
+    end
+  end
+end
---
+++ spec/csv_importer_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+describe 'CsvImporter' do
+  describe '#import' do
+    it 'imports the data correctly' do
+      CsvImporter.import 'spec/fixtures/test_data.csv'
+      expect(Incident.all.count).to eq 2
+
+      first_incident = Incident.first
+      expect(first_incident.victim_name).to eq "Garrett Chruma"
+      expect(first_incident.victim_age).to eq 21
+      expect(first_incident.victim_gender).to eq "Male"
+      expect(first_incident.victim_race).to eq "Asian"
+      expect(first_incident.victim_image_url).to eq "https://usgunviolence.files.wordpress.com/2014/03/garett-chruma.jpg?w=625"
+      expect(first_incident.incident_date).to eq Date.parse("6/8/2013")
+      expect(first_incident.incident_street_address).to eq "Mason Ferry Road"
+      expect(first_incident.incident_city).to eq "Wilmer"
+      expect(first_incident.incident_state).to eq "AL"
+      expect(first_incident.incident_zip).to eq "36587"
+      expect(first_incident.incident_county).to eq "Mobile"
+      expect(first_incident.agency_responsible).to eq "Alabama State Police"
+      expect(first_incident.cause_of_death).to eq "Gunshot"
+      expect(first_incident.alleged_victim_crime).to eq nil
+      expect(first_incident.crime_category).to eq nil
+      expect(first_incident.aggregate_crime_category).to eq nil
+      expect(first_incident.caveat).to eq nil
+      expect(first_incident.solution).to eq nil
+      expect(first_incident.incident_description).to eq 'Alabama State Troopers began a "short, high speed chase" of Garrett Chruma, 21, armed on motorcycle. The chase continued on foot, ended with a confrontation between officers in which Chruma was shot and killed, near Mason Ferry Road in Wilmer.The Alabama Bureau of Investigation (ABI) is conducting an investigation.'
+      expect(first_incident.official_disposition_of_death).to eq "Pending investigation"
+      expect(first_incident.criminal_charges).to eq "No Known Charges"
+      expect(first_incident.news_url).to eq "http://www.abc3340.com/story/22548035/state-trooper-shoots-kills-suspect-after-chase-near-mobile"
+      expect(first_incident.mental_illness).to eq "No"
+      expect(first_incident.unarmed).to eq "Allegedly Armed"
+      expect(first_incident.line_of_duty).to eq "Line of Duty"
+      expect(first_incident.unique_mpv).to eq 457
+    end
+  end
+end
