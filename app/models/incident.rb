f5481027a2a4210d8b7f2338f4fda41ec292aacf
Laura Kogler <lkogler@gmail.com>
2015-08-25 21:46:54 -0700

    testing cache expiration. Remove cache from git.

--- app/models/incident.rb
+++ app/models/incident.rb
@@ -1,2 +1,8 @@
 class Incident < ActiveRecord::Base
+  after_update :expire_cache
+
+  def expire_cache
+    print "expiring cache!"
+    ActionController::Base.expire_page('api/v1/incidents.json')
+  end
 end
