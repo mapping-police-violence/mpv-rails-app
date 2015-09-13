5a09e8124cbc5d42caaa0af7a7e297f75a85f03a
Laura Kogler <lkogler@gmail.com>
2015-09-12 18:18:00 -0700

    [#101362828] Add CSV import method for The Counted csv files

--- app/helpers/csv_importer.rb
+++ app/helpers/csv_importer.rb
@@ -3,7 +3,7 @@ require 'csv'
 class CsvImporter
   def self.import filename
     CSV.foreach filename do |row|
-      next if row[0] == "Id"
+      next if row[0].nil? || row[0] == "Id"
 
       Incident.create!({
                           :victim_name => row[1],
@@ -39,6 +39,48 @@ class CsvImporter
                           :longitude => row[33]
                       })
 
+    end
+  end
+
+  def self.import_the_counted filename
+    CSV.foreach filename do |row|
+      next if row[0].nil? || row[0] == "name"
+
+      Incident.create!({
+                           :victim_name => row[0],
+                           :victim_age => row[1],
+                           :victim_gender => row[2],
+                           :victim_race => parse_race(row[3]),
+                           :incident_date => parse_date(row[4],row[5],row[6]),
+                           :incident_street_address => row[7],
+                           :incident_city => row[8],
+                           :incident_state => row[9],
+                           :cause_of_death => row[10],
+                           :agency_responsible => row[11],
+                           :unarmed => parse_unarmed(row[12])
+                       })
+    end
+  end
+
+  def self.parse_race input
+    if input == 'Hispanic/Latino'
+      'Hispanic'
+    else
+      input
+    end
+  end
+
+  def self.parse_date(month, day, year)
+    Date.parse("#{month} #{day}, #{year}")
+  end
+
+  def self.parse_unarmed input
+    if input == 'No' || input == 'Non-lethal firearm'
+      'Unarmed'
+    elsif input == 'Unknown'
+      'Unclear'
+    else
+      'Allegedly Armed'
     end
   end
 
