50bf8ef2f38401b5a0968368aae65b21919ac92d
tilde <tilde@pinterest.com>
2015-12-13 18:30:24 -0800

    [#109230346] Add de-duplication of entries by name and date for the Counted.

--- app/utils/counted_importer.rb
+++ app/utils/counted_importer.rb
@@ -11,19 +11,28 @@ class CountedImporter < DataImporter
   end
 
   def self.import_row row
-    Incident.create!({
-                         :victim_name => row[0],
-                         :victim_age => row[1],
-                         :victim_gender => row[2],
-                         :victim_race => parse_race(row[3]),
-                         :incident_date => parse_date(row[4],row[5],row[6]),
-                         :incident_street_address => row[7],
-                         :incident_city => row[8],
-                         :incident_state => row[9],
-                         :cause_of_death => row[10],
-                         :agency_responsible => row[11],
-                         :unarmed => parse_unarmed(row[12])
-                     })
+    date = parse_date(row[4],row[5],row[6])
+    duplicate_entries = Incident.where(:victim_name => row[0], :incident_date => date-3.days..date+3.days)
+
+    params = {
+        :victim_name => row[0],
+        :victim_age => row[1],
+        :victim_gender => row[2],
+        :victim_race => parse_race(row[3]),
+        :incident_date => date,
+        :incident_street_address => row[7],
+        :incident_city => row[8],
+        :incident_state => row[9],
+        :cause_of_death => row[10],
+        :agency_responsible => row[11],
+        :unarmed => parse_unarmed(row[12])
+    }
+
+    if duplicate_entries.count > 0
+      duplicate_entries.first.update!(params)
+    else
+      Incident.create!(params)
+    end
   end
 
   def self.parse_race input
--- spec/importers/counted_importer_spec.rb
+++ spec/importers/counted_importer_spec.rb
@@ -55,6 +55,51 @@ describe 'CountedImporter' do
     end
   end
 
+  it 'should overwrite data for all fields for any existing entry with same name within 3 days' do
+    Incident.create( :victim_name => "Richard Carlin",
+                      :victim_age =>36,
+                      :victim_gender => "Female",
+                      :victim_race => "Black",
+                      :incident_date => "February 15,2015,",
+                      :incident_street_address => "4141 Rehr St",
+                      :incident_city => "Reeeading",
+                      :incident_state => "GA",
+                      :cause_of_death => "Suffocation",
+                      :agency_responsible => "Georgia State Police",
+                      :unarmed => "Allegedly Armed",
+                      :incident_description => "lorem ipsum",
+                      :unique_mpv => 7
+                    )
+
+    Incident.create( :victim_name => "Matthew Hoffman",
+                     :incident_date => "January 8,2015,"
+    )
+    file = File.new('spec/fixtures/the_counted_data.csv')
+    CountedImporter.import file
+    expect(Incident.all.count).to eq 5
+
+    expect(Incident.where(:victim_name => "Richard Carlin").count).to eq 1
+    first_incident = Incident.where(:victim_name => "Richard Carlin").first
+    expect(first_incident.victim_age).to eq 35
+    expect(first_incident.victim_gender).to eq "Male"
+    expect(first_incident.victim_race).to eq "White"
+    expect(first_incident.victim_image_url).to eq nil
+    expect(first_incident.incident_date).to eq Date.parse("2015/02/13")
+    expect(first_incident.incident_street_address).to eq "414 Rehr St"
+    expect(first_incident.incident_city).to eq "Reading"
+    expect(first_incident.incident_state).to eq "PA"
+    expect(first_incident.incident_zip).to eq nil
+    expect(first_incident.incident_county).to eq nil
+    expect(first_incident.agency_responsible).to eq "Pennsylvania State Police"
+    expect(first_incident.cause_of_death).to eq "Gunshot"
+    expect(first_incident.incident_description).to eq "lorem ipsum"
+    expect(first_incident.unarmed).to eq "Unclear"
+    expect(first_incident.line_of_duty).to eq nil
+    expect(first_incident.unique_mpv).to eq 7
+
+    expect(Incident.where(:victim_name => "Matthew Hoffman").count).to eq 2
+  end
+
   after(:all) do
     Incident.delete_all
   end
