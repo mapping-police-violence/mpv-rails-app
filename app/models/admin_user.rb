396a8c56a59d3c81bb933b593120237851cc5e9c
Sarah Tappon <sarah.tappon@gmail.com>
2016-01-05 22:12:08 -0800

    [#109230414] Fix bugs and add tests for audit trail (Laura & Sarah)

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -3,4 +3,8 @@ class AdminUser < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, 
          :recoverable, :rememberable, :trackable, :validatable
+
+  def self.get_display_name(id)
+    id ? AdminUser.find(id).email : 'Unknown user'
+  end
 end
--- spec/features/admin_incidents_spec.rb
+++ spec/features/admin_incidents_spec.rb
@@ -29,6 +29,23 @@ describe 'the incidents page' do
   it 'shows the incident version information' do
     click_link 'Mohammad Abdulazeez'
     expect(page).to have_content 'This is the latest version'
+    expect(page).not_to have_content 'Other versions'
+    click_link 'Edit Incident'
+    fill_in 'Victim age', :with => '45'
+    click_button 'Update Incident'
+    expect(page).to have_content 'This is the latest version'
+    expect(page).to have_content 'By: admin@example.com'
+    expect(page).to have_content 'Other versions'
+    click_link 'Version 1'
+    expect(page).to have_content 'This is version 1'
+    expect(page).to have_link 'Latest'
+
+    click_link 'View History'
+    expect(page.find('#diff-version-1')).to have_content 'Version 1'
+    expect(page.find('#diff-version-2')).to have_content 'Version 2'
+    expect(page.find('#diff-version-2')).to have_content 'victim_age:'
+    expect(page.find('#diff-version-2')).to have_content '- 24'
+    expect(page.find('#diff-version-2')).to have_content '- 45'
   end
 end
 
