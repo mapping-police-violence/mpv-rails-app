e4b467b513cefea8d85571470bdbd3dbb1de6872
Sarah Tappon <sarah.tappon@gmail.com>
2016-02-02 21:16:33 -0800

    [#109230406] standardize formatting and reduce code duplication in download page spec

--- spec/features/admin_download_page_spec.rb
+++ spec/features/admin_download_page_spec.rb
@@ -4,63 +4,58 @@ describe 'the download page' do
   before :each do
     AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
     visit '/admin/incidents/download_file'
-    fill_in "Email", :with => "admin@example.com"
-    fill_in "Password", :with => "password"
-    click_button "Login"
+    fill_in 'Email', :with => 'admin@example.com'
+    fill_in 'Password', :with => 'password'
+    click_button 'Login'
   end
 
-  it 'allows users to download all entries that need review' do
-    needs_review = Incident.create!(:victim_name => 'Needs Review')
-    does_not_need_review = Incident.create!(:needs_review => false,
-                                            :victim_name => 'Reviewed Already')
-    check('Only incidents that need review')
-    click_button 'Download'
-
+  def check_header(page)
     header = page.response_headers['Content-Disposition']
     expect(header).to match /^attachment/
     expect(header).to match /filename="incidents-.+\.csv"$/
+  end
+
+  it 'allows users to download all entries that need review' do
+    Incident.create!(:victim_name => 'Needs Review')
+    Incident.create!(:needs_review => false,
+                     :victim_name => 'Reviewed Already')
+    check('Only incidents that need review')
+    click_button 'Download'
+
+    check_header(page)
 
     expect(page).to have_content('Needs Review')
     expect(page).not_to have_content('Reviewed Already')
   end
 
   it 'downloads everything when no filter options are selected' do
-    needs_review = Incident.create!(:victim_name => 'Needs Review')
-    does_not_need_review = Incident.create!(:needs_review => false,
-                                            :victim_name => 'Reviewed Already')
+    Incident.create!(:victim_name => 'Needs Review')
+    Incident.create!(:needs_review => false,
+                     :victim_name => 'Reviewed Already')
     click_button 'Download'
 
-    header = page.response_headers['Content-Disposition']
-    expect(header).to match /^attachment/
-    expect(header).to match /filename="incidents-.+\.csv"$/
+    check_header(page)
 
     expect(page).to have_content('Needs Review')
     expect(page).to have_content('Reviewed Already')
   end
 
-  it ('allows users to download entries after a given date') do
+  it ('allows users to filter entries by creation date') do
 
-    Incident.create!({
-                        :victim_name => 'Current Test Entry'
-                    })
+    Incident.create!(:victim_name => 'Current Test Entry')
 
     Timecop.freeze(Time.local(2000)) do
-      Incident.create!({
-                           :victim_name => 'Older Test Entry'
-                       })
+      Incident.create!(:victim_name => 'Older Test Entry')
     end
 
     fill_in 'Entries after:', :with => '2015-01-01'
     click_button 'Download'
 
-    header = page.response_headers['Content-Disposition']
-    expect(header).to match /^attachment/
-    expect(header).to match /filename="incidents-.+\.csv"$/
+    check_header(page)
 
     expect(page).to have_content('Current Test Entry')
     expect(page).not_to have_content('Older Test Entry')
   end
-
 
 end
 
